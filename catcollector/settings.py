f55e048cea6ab914b891ec2c3dfb5005fcfd88d4
gonczarM <mattisagumball@gmail.com>
2023-01-05 15:16:07 -0700

    deployment ready

--- catcollector/settings.py
+++ catcollector/settings.py
@@ -128,3 +128,7 @@ STATIC_URL = '/static/'
 LOGIN_REDIRECT_URL = '/cats/'
 
 LOGOUT_REDIRECT_URL = '/'
+
+# Configure Django App for Heroku.
+import django_on_heroku
+django_on_heroku.settings(locals())
